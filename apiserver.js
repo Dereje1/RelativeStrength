9086b3e1370e5dad02b0b5d18cf93b77093ff2c2
Dereje1 <getahun.d@gmail.com>
2018-05-11 10:30:20 +0300

    include dotenv

--- apiserver.js
+++ apiserver.js
@@ -1,4 +1,5 @@
 "use strict"//primary module to interact with client
+const priv = require('dotenv').config();
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
