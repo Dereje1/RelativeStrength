ca825de3d3fa27acbdac5993ce031d8cfb2551c1
Dereje1 <getahun.d@gmail.com>
2018-12-31 17:29:41 +0700

    ff calendar new address

--- server/fromAWS/calendar.js
+++ server/fromAWS/calendar.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 module.exports = () =>
   new Promise((resolve, reject) => {
-    axios.get('https://cdn-nfs.forexfactory.net/ff_calendar_thisweek.json')
+    axios.get('https://cdn-nfs.faireconomy.media/ff_calendar_thisweek.json')
       .then((response) => {
         resolve({ weeklyevents: response.data });
       })
